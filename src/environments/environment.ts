1fa614436dcd716b97aa06d5c2f4abc3768ae7cf
Mukesh Kumar <ak9991927952@gmail.com>
2022-10-28 20:12:15 +0530

    on production mode

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig: {
     apiKey: "AIzaSyA1qxnVDWH_emhU6tfv5YvCuz7hWoMd2J8",
     authDomain: "compact-cell-317515.firebaseapp.com",
@@ -11,7 +11,7 @@ export const environment = {
     storageBucket: "compact-cell-317515.appspot.com",
     messagingSenderId: "511114061529",
     appId: "1:511114061529:web:595486b059f117b8cf80c8"
-  }
+  },
 };
 /*
  * For easier debugging in development mode, you can import the following file
